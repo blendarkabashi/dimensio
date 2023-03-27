7074cf3c8a74d1e01566e3124d5303985656c5fa
Blendar Kabashi <blendar.h.kabashi@gmail.com>
2023-03-27 23:57:05 +0200

    register form work

--- components/select/chipSelect/index.js
+++ components/select/chipSelect/index.js
@@ -1,4 +1,7 @@
 import Chip from 'components/chip';
+import StarBorderRounded from '@mui/icons-material/StarBorderRounded';
+import StarRounded from '@mui/icons-material/StarRounded';
+import { useEffect } from 'react';
 const ChipSelect = ({
   className,
   label,
@@ -31,35 +34,71 @@ const ChipSelect = ({
       selectedOptions.filter((chip) => chip.name !== chipToRemove)
     );
   };
-  return (
-    <div className={`${className} cursor-text`}>
-      {label && <div className="mb-2">{label}</div>}
-      <div
-        onClick={() => document.querySelector('.tags-input').focus()}
-        className={`flex flex-wrap items-center border-darkGray rounded-md w-full border px-3 py-2 bg-white py-0 min-h-[40px] outline-none `}
-      >
-        {selectedOptions.map((selection, index) => (
-          <Chip
-            color="bg-blue"
-            key={index}
-            className="text-white mr-1 my-1 text-base"
-            closable
-            removeChip={removeChip}
-          >
-            {selection.name}
-          </Chip>
-        ))}
-        <input
-          className="tags-input border-none outline-none h-full focus:ring-transparent focus:border-none w-[220px] p-0"
-          type="text"
-          placeholder="Add stack and press enter..."
-          onKeyDown={(event) => handleAddChip(event)}
-          // onInput={onInput}
-          // onChange={onChange}
-          // onKeyDown={onKeyDown}
-          // value={value}
+
+  const calculateStars = (techStack) => {
+    let stars = [];
+    for (let i = 0; i < techStack.yearsOfExperience; i++) {
+      stars.push(
+        <StarRounded key={i} className="cursor-pointer text-orange w-7 h-7" />
+      );
+    }
+    for (let i = techStack.yearsOfExperience; i < 5; i++) {
+      stars.push(
+        <StarBorderRounded
+          key={i}
+          className="cursor-pointer text-orange w-7 h-7"
         />
+      );
+    }
+    return stars;
+  };
+  return (
+    <div className={className}>
+      <div className={`cursor-text`}>
+        {label && <div className="mb-2">{label}</div>}
+        <div
+          onClick={() => document.querySelector('.tags-input').focus()}
+          className={`flex flex-wrap items-center border-darkGray rounded-md w-full border px-3 py-2 bg-white py-0 min-h-[40px] outline-none `}
+        >
+          {selectedOptions.map((selection, index) => (
+            <Chip
+              color="bg-blue"
+              key={index}
+              className="text-white mr-1 my-1 text-base"
+              closable
+              removeChip={removeChip}
+            >
+              {selection.name}
+            </Chip>
+          ))}
+          <input
+            className="tags-input border-none outline-none h-full focus:ring-transparent focus:border-none w-[220px] p-0"
+            type="text"
+            placeholder="Add stack and press enter..."
+            onKeyDown={(event) => handleAddChip(event)}
+            // onInput={onInput}
+            // onChange={onChange}
+            // onKeyDown={onKeyDown}
+            // value={value}
+          />
+        </div>
       </div>
+      {/* {selectedOptions.length > 0 && (
+        <div className="mt-5">
+          <div className="flex items-center mb-2">
+            <div className="w-1/3 font-medium">Tech Stack</div>
+            <div className="w-2/3 font-medium">Years of Experience</div>
+          </div>
+          {selectedOptions.map((techStack) => (
+            <div className="flex items-center">
+              <div className="w-1/3">{techStack.name}</div>
+              <div className="w-2/3 flex items-center">
+                {calculateStars(techStack)}
+              </div>
+            </div>
+          ))}
+        </div>
+      )} */}
     </div>
   );
 };
--- pages/job-seeker/register/index.js
+++ pages/job-seeker/register/index.js
@@ -9,7 +9,9 @@ import Dropdown from 'components/dropdown';
 import DaysOfWorkSelect from 'components/select/days-of-work';
 import ChipSelect from 'components/select/chipSelect';
 import TechStackExperience from 'components/techStackExperience';
+import MultiStepForm from 'components/multi-step-form';
 const index = () => {
+  const [currentStep, setCurrentStep] = useState(1);
   const [techStack, setTechStack] = useState([]);
   const [techStackExperience, setTechStackExperience] = useState([]);
   const [option, setOption] = useState(0);
@@ -29,6 +31,12 @@ const index = () => {
       name: 'Big (40+ workers)',
     },
   ];
+
+  const steps = [
+    { id: 1, name: 'Job details', completed: true, href: '#' },
+    { id: 2, name: 'Application form', completed: false, href: '#' },
+    { id: 3, name: 'Preview', completed: false, href: '#' },
+  ];
   return (
     <div className="my-[80px]">
       <div className="container">
@@ -36,14 +44,72 @@ const index = () => {
           <Image src={logo}></Image>
         </div>
         <div className="flex items-center justify-between">
-          <div className="max-w-[400px]">
+          <div className="min-w-[420px]">
             <div className="text-5xl font-medium mb-3 text-center">
               Create account
             </div>
             <div className="text-darkGray text-xl mb-[50px] text-center">
               Please fill in the inputs to register.
             </div>
-            <Input
+            <div
+              className="absolute top-0 right-0 hidden h-full w-5 md:block"
+              aria-hidden="true"
+            >
+              <svg
+                className="h-full w-full text-gray-300"
+                viewBox="0 0 22 80"
+                fill="none"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M0 -2L20 40L0 82"
+                  vectorEffect="non-scaling-stroke"
+                  stroke="currentcolor"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <MultiStepForm
+              steps={steps}
+              currentStep={currentStep}
+              setCurrentStep={setCurrentStep}
+            >
+              {currentStep == 1 ? (
+                <div className="step">
+                  <Input
+                    type="file"
+                    id="profile-picture"
+                    label="Upload Profile Picture"
+                    placeholder="Attach your profile picture"
+                    className="mb-5"
+                  />
+                  <Input
+                    type="textarea"
+                    label="Biography"
+                    placeholder="Write your bio"
+                    className="mb-5"
+                  />
+                  <Input
+                    type="text"
+                    label="Full Name"
+                    className="mb-5"
+                    placeholder="Blendar Kabashi"
+                  />
+                  <Input type="date" label="Date of Birth" className="mb-5" />
+                  <Input
+                    type="text"
+                    label="Location"
+                    className="mb-5"
+                    placeholder="Prishtine, Kosova"
+                  />
+                </div>
+              ) : currentStep == 2 ? (
+                <div className="step">step 2</div>
+              ) : (
+                <div className="step">step 3</div>
+              )}
+            </MultiStepForm>
+            {/* <Input
               type="text"
               label="Email"
               className="mb-5"
@@ -76,21 +142,13 @@ const index = () => {
               selectedOptions={techStack}
               setSelectedOptions={setTechStack}
             />
-            {techStack.length > 0 && (
-              <TechStackExperience options={techStack} className="mb-5" />
-            )}
             <Input type="date" label="Founding Date" className="mb-5" />
             <Input
               type="file"
               id="fileupload1"
               label="Upload Document"
               className="mb-5"
-            />
-            <div className="flex items-center justify-end">
-              <Button className="px-10" type="submit">
-                Next Step
-              </Button>
-            </div>
+            /> */}
           </div>
           <div className="h-[550px] w-[400px]">
             <Image className="h-full w-full" src={lookingIllustration} />
